44b5b1050a607133aa15674252f0519ed713ea4c
AnleAnja <anleanja@live.de>
2020-07-29 16:26:30 +0200

    add transpiler and minification to development mode

--- Frontend/dev/.docker/development/entrypoint.js
+++ Frontend/dev/.docker/development/entrypoint.js
@@ -1,5 +1,6 @@
 const { spawn } = require('child_process');
 const sassWatcher = spawn('sass', ['--watch', 'assets/scss/styles.scss:public/css/styles.min.css', '--style=compressed']);
+const babelWatcher = spawn('npx', ['babel', 'assets/js/main.js', '-w', '-o', 'public/js/main.min.js']);
 const startApp = spawn('supervisor', ['src/app.js']);
 
 sassWatcher.stdout.on('data', (data) => {
@@ -23,5 +24,17 @@ startApp.stderr.on('data', (data) => {
 });
 
 startApp.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
+babelWatcher.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+babelWatcher.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+babelWatcher.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
 });
--- Frontend/dev/assets/js/main.js
+++ Frontend/dev/assets/js/main.js
@@ -1,3 +1,3 @@
 console.log('Hello World');
 // eslint-disable-next-line no-undef
-alert('Hello World');
+alert('Hallo Welt');
