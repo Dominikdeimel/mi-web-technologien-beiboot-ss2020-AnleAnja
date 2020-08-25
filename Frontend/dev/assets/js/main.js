1b217a894a74593b69c3a3a481143f90ea6adf65
Dominik Deimel <domi.deimel@yahoo.de>
2020-08-26 00:41:47 +0200

    Display img with quote and gradient added

--- Frontend/dev/assets/js/main.js
+++ Frontend/dev/assets/js/main.js
@@ -1,8 +1,20 @@
-$(document).ready( function(){
-    $('button').bind('click', function(){
-        $.get( 'http://localhost:3000/random', function( data ) {
-            console.log(data);
-            $('.i1').attr('src', `http://localhost:3000/image/${data.image}`);
-        });
+async function main(){
+    const button = document.getElementById('button');
+    button.addEventListener('click', await loadImage());
+}
+
+async function loadImage(){
+    const request = new Request('http://localhost:3000/random');
+    const result = await fetch(request);
+    console.log(result);
+    // $('.i1').attr('src', `http://localhost:3000/image/${data.image}`);
+}
+
+
+/* async function getQuote(){
+    $.get('https://quotes.rest/qod', function ( data ) {
+        document.getElementById('quote').innerText = '';
     });
-});
+} */
+
+main();
