460e534f38de62d8609c61edfc206524020a0d88
Dominik Deimel <domi.deimel@yahoo.de>
2020-08-31 17:49:42 +0200

    Mobile support added

--- Frontend/dev/public/js/status.js
+++ Frontend/dev/public/js/status.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
@@ -6,14 +5,4 @@ function init() {
         const statusElem = document.querySelector('.page-status');
         statusElem.innerHTML = 'offline';
     }
-=======
-document.addEventListener('DOMContentLoaded', init, false);
-
-function init() {
-    if (!navigator.onLine) {
-        const statusElem = document.querySelector('.page-status');
-        statusElem.innerHTML = 'offline';
-        
-    }
->>>>>>> 8f22e132f8136e18a8d6b39d585704b722e304cb
 }
